c7ae3533c71bd19d3cdbe5a53a71ed916071d32a
Ricardo Chávez <75815255+TheWhiteLizard@users.noreply.github.com>
2022-07-10 17:58:13 -0500

    Vista de Perfil Terminada (Solo lo visual)

--- app/src/main/java/com/example/recomed/UserProfile.java
+++ app/src/main/java/com/example/recomed/UserProfile.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class perfil_cerrar_sesion_GOD extends AppCompatActivity {
+public class UserProfile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_perfil_cerrar_sesion_god);
+        setContentView(R.layout.activity_user_profile);
     }
 }
