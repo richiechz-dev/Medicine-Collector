10b0aa53a9c62a31460731d9633ff719746a25f6
Ricardo Chávez <75815255+TheWhiteLizard@users.noreply.github.com>
2022-07-09 23:19:52 -0500

    Layout Perfil_cerrar_sesion_GOD

---
+++ app/src/main/java/com/example/recomed/Perfil_cerrar_sesion_GOD.java
@@ -0,0 +1,14 @@
+package com.example.recomed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Perfil_cerrar_sesion_GOD extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_cerrar_sesion_god);
+    }
+}
