9f1664706efb07ae4c4fc9631adf4c8985dc0d1f
aalexElrick <62070466+aalexElrick@users.noreply.github.com>
2022-07-10 20:29:02 -0500

    boton de regreso principal

--- app/src/main/java/com/example/recomed/principal.java
+++ app/src/main/java/com/example/recomed/principal.java
@@ -2,13 +2,29 @@ package com.example.recomed;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class principal extends AppCompatActivity {
+
+    Button RegresarP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        RegresarP= findViewById(R.id.btnRegresarR);
+
+        RegresarP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(principal.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
+
 }
