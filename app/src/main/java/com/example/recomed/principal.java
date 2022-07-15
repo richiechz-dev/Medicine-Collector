0aa8da6a598787d9edae637cf31f442a574862bb
Jenni_uwu <jennigac3@gmail.com>
2022-07-15 18:58:26 -0500

    correción del captcha

--- app/src/main/java/com/example/recomed/principal.java
+++ app/src/main/java/com/example/recomed/principal.java
@@ -9,21 +9,12 @@ import android.widget.Button;
 
 public class principal extends AppCompatActivity {
 
-    Button RegresarP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
-        RegresarP= findViewById(R.id.RegresarP);
 
-        RegresarP.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(principal.this, MainActivity.class);
-                startActivity(i);
-            }
-        });
 
     }
 
