37e76670c1a9b46750b6ef5d489ae56c86f3d19d
Jenni_uwu <jennigac3@gmail.com>
2022-07-15 19:07:03 -0500

    correción del captcha

---
+++ app/src/main/java/com/example/recomed/paraactua.java
@@ -0,0 +1,14 @@
+package com.example.recomed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class paraactua extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paraactua);
+    }
+}
